1d67aa5659ff8e5eda5410b1b3c4d30392815b1a
Maxim Danilchenko <dmax.dev@gmail.com>
2018-10-23 23:15:38 +0300

    benchmarks for future speedups

---
+++ benchmarks.py
@@ -0,0 +1,96 @@
+"""
+This module is used for testing speed of aiochclient after
+any changes in its code. It is useful for comparing its
+results with older versions speed. Results mostly depends
+on the part which makes serialize and deserialize part of work
+"""
+import time
+import datetime as dt
+from asyncio import run
+
+from aiohttp import ClientSession
+from aiochclient import ChClient
+
+
+def row_data():
+    return (
+        1,
+        2,
+        3.14,
+        "hello",
+        "world",
+        dt.date.today(),
+        dt.datetime.utcnow(),
+        "hello",
+        (3, "world"),
+        None,
+        ["q", "w", "e", "r"],
+    )
+
+
+async def prepare_db(client):
+    await client.execute("DROP TABLE IF EXISTS benchmark_tbl")
+    await client.execute(
+        """
+        CREATE TABLE benchmark_tbl ( 
+                            a UInt16,
+                            b Int16,
+                            c Float32,
+                            d String,
+                            e FixedString(16),
+                            f Date,
+                            g DateTime,
+                            h Enum16('hello' = 1, 'world' = 2),
+                            i Tuple(UInt8, String),
+                            j Nullable(Int8),
+                            k Array(String)
+        ) ENGINE = Memory
+        """
+    )
+
+
+async def insert_rows(client, test_data, number):
+    await client.execute(
+        "INSERT INTO benchmark_tbl VALUES", *(test_data for _ in range(number))
+    )
+
+
+async def bench_selects(*, retries: int, rows: int):
+    async with ClientSession() as s:
+        client = ChClient(s, compress_response=True)
+        # prepare environment
+        await prepare_db(client)
+        await insert_rows(client, row_data(), rows)
+        # actual testing
+        times = []
+        for _ in range(retries):
+            start_time = time.time()
+            await client.fetch("SELECT * FROM benchmark_tbl")
+            times.append(time.time() - start_time)
+    print(
+        f"Average time for selecting {rows} rows (from {retries} runs): {sum(times)/retries} seconds"
+    )
+
+
+async def bench_inserts(*, retries: int, rows: int):
+    async with ClientSession() as s:
+        client = ChClient(s, compress_response=True)
+        # prepare environment
+        await prepare_db(client)
+        # actual testing
+        times = []
+        one_row = row_data()
+        for _ in range(retries):
+            start_time = time.time()
+            await client.execute(
+                "INSERT INTO benchmark_tbl VALUES", *(one_row for _ in range(rows))
+            )
+            times.append(time.time() - start_time)
+    print(
+        f"Average time for inserting {rows} rows (from {retries} runs): {sum(times)/retries} seconds"
+    )
+
+
+if __name__ == "__main__":
+    run(bench_selects(retries=1000, rows=1000))
+    run(bench_inserts(retries=1000, rows=1000))
