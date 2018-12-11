5e8f1f69ce400104490b846963dc833272428a00
Maxim Danilchenko <dmax.dev@gmail.com>
2018-12-11 22:50:52 +0300

    UUID in README and in exception about types + closed: #2 (implemented in 2f74c3e5e2fcd101d2a78a5381f97c2935f87556)

--- aiochclient/types.py
+++ aiochclient/types.py
@@ -294,7 +294,7 @@ def py2ch(value):
         raise ChClientError(
             f"Unrecognized type: '{type(value)}'. "
             f"The value type should be exactly one of "
-            f"int, float, str, dt.date, dt.datetime, tuple, list (or None). "
+            f"int, float, str, dt.date, dt.datetime, tuple, list, uuid.UUID (or None). "
             f"No subclasses yet."
         )
 
