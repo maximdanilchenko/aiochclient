d5102def2ba7bfeee505ad351099c0dd65c37aa0
Максим Данильченко <m.danilchenko@atol.ru>
2019-05-14 19:02:53 +0300

    New benchmarks - up to 10x improve in select with lazy decoding

--- benchmarks.py
+++ benchmarks.py
@@ -72,7 +72,26 @@ async def bench_selects(*, retries: int, rows: int):
         total_time = time.time() - start_time
         avg_time = total_time / retries
     print(
-        f"- Average time for selecting {rows} rows (from {retries} runs): {avg_time} seconds. Total: {total_time}"
+        f"- Avg time for selecting {rows} rows from {retries} runs: {avg_time} sec. Total: {total_time}"
+    )
+
+
+async def bench_selects_with_decoding(*, retries: int, rows: int):
+    async with ClientSession() as s:
+        client = ChClient(s, compress_response=True)
+        # prepare environment
+        await prepare_db(client)
+        await insert_rows(client, row_data(), rows)
+        # actual testing
+        start_time = time.time()
+        for _ in range(retries):
+            selected_rows = await client.fetch("SELECT * FROM benchmark_tbl")
+            # decoding:
+            selected_rows = [row[0] for row in selected_rows]
+        total_time = time.time() - start_time
+        avg_time = total_time / retries
+    print(
+        f"- Avg time for selecting {rows} rows from {retries} runs: {avg_time} sec (with decoding). Total: {total_time}"
     )
 
 
@@ -91,12 +110,13 @@ async def bench_inserts(*, retries: int, rows: int):
         total_time = time.time() - start_time
         avg_time = total_time / retries
     print(
-        f"- Average time for inserting {rows} rows (from {retries} runs): {avg_time} seconds. Total: {total_time}"
+        f"- Avg time for inserting {rows} rows from {retries} runs: {avg_time} sec. Total: {total_time}"
     )
 
 
 async def main():
     await bench_selects(retries=1000, rows=1000)
+    await bench_selects_with_decoding(retries=1000, rows=1000)
     await bench_inserts(retries=1000, rows=1000)
 
 
@@ -105,11 +125,23 @@ if __name__ == "__main__":
     loop.run_until_complete(main())
     loop.close()
 """
+Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 03:13:28)
 Pure Python:
-- Average time for selecting 1000 rows (from 1000 runs): 0.09247343492507934 seconds
-- Average time for inserting 1000 rows (from 1000 runs): 0.032303282737731934 seconds
+- Avg time for selecting 1000 rows from 1000 runs: 0.09247343492507934 sec
+- Avg time for inserting 1000 rows from 1000 runs: 0.032303282737731934 sec
 
 With Cython ext:
-- Average time for selecting 1000 rows (from 1000 runs): 0.061105722188949586 seconds
-- Average time for inserting 1000 rows (from 1000 runs): 0.01820656132698059 seconds
+- Avg time for selecting 1000 rows from 1000 runs: 0.061105722188949586 sec
+- Avg time for inserting 1000 rows from 1000 runs: 0.01820656132698059 sec
+
+With lazy decoding:
+Pure Python:
+- Avg time for selecting 1000 rows from 1000 runs: 0.00917806601524353 sec.
+- Avg time for selecting 1000 rows from 1000 runs: 0.08189528131484985 sec (with decoding).
+- Avg time for inserting 1000 rows from 1000 runs: 0.027863742113113404 sec.
+
+With Cython ext:
+- Avg time for selecting 1000 rows from 1000 runs: 0.009102407217025757 sec.
+- Avg time for selecting 1000 rows from 1000 runs: 0.05832741093635559 sec (with decoding).
+- Avg time for inserting 1000 rows from 1000 runs: 0.01853747296333313 sec.
 """
