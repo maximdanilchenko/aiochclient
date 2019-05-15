43c83dbb7f5f3c5eab886f31d26ca17bff291e10
Maxim Danilchenko <dmax.dev@gmail.com>
2019-05-15 19:57:22 +0200

    Record class refactoring

--- aiochclient/records.py
+++ aiochclient/records.py
@@ -9,22 +9,28 @@ try:
 except ImportError:
     from aiochclient.types import what_py_converter
 
-__all__ = ["RecordsFabric"]
+__all__ = ["RecordsFabric", "Record"]
 
 
 class Record(Mapping):
 
-    __slots__ = ("_row", "_names", "_decoded", "_converters")
+    __slots__ = ("_converters", "_decoded", "_names", "_row")
 
     def __init__(
         self, row: Tuple[bytes], names: Dict[str, Any], converters: List[Callable]
     ):
-        if row and len(row) != len(names):
-            raise ChClientError("Incorrect response from ClickHouse")
         self._row: Tuple[Any] = row
-        self._decoded = False
-        self._names = names
-        self._converters = converters
+        if not self._row:
+            # in case of empty row
+            self._decoded = True
+            self._converters = []
+            self._names = {}
+        else:
+            if len(row) != len(names):
+                raise ChClientError("Incorrect response from ClickHouse")
+            self._decoded = False
+            self._converters = converters
+            self._names = names
 
     def __getitem__(self, key: Union[str, int, slice]) -> Any:
         if not self._decoded:
@@ -33,27 +39,24 @@ class Record(Mapping):
             try:
                 return self._row[self._names[key]]
             except KeyError:
-                raise KeyError(f"No fields with this name: {key}")
-        elif type(key) in (int, slice):
-            try:
-                return self._row[key]
-            except IndexError:
-                raise IndexError(f"No fields with this index: {key}")
-        else:
-            raise TypeError(f"Incorrect key/index type: {type(key)}")
+                if not self._row:
+                    raise KeyError("Empty row. May be it is result of 'WITH TOTALS' query.")
+                raise KeyError(f"No fields with name '{key}'")
+        try:
+            return self._row[key]
+        except IndexError:
+            if not self._row:
+                raise IndexError("Empty row. May be it is result of 'WITH TOTALS' query.")
+            raise IndexError(f"No fields with index '{key}'")
 
     def __iter__(self) -> Iterator:
-        if self._row:
-            return iter(self._names)
-        else:
-            return iter(())
+        return iter(self._names)
 
     def __len__(self) -> int:
         return len(self._row)
 
     def _decode(self):
-        if self._row:
-            self._row = tuple(typ(val) for typ, val in zip(self._converters, self._row))
+        self._row = tuple(typ(val) for typ, val in zip(self._converters, self._row))
         self._decoded = True
 
 
@@ -68,7 +71,7 @@ class RecordsFabric:
             what_py_converter(tp) for tp in tps.decode().strip().split("\t")
         ]
 
-    def new(self, vls: bytes):
+    def new(self, vls: bytes) -> Record:
         vls = vls[:-1]
         return Record(
             row=tuple(vls.split(b"\t")) if vls else (),
