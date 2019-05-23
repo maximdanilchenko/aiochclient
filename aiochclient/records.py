96ab772a140f895359171995ac98fe25b6464c8b
Максим Данильченко <m.danilchenko@atol.ru>
2019-05-23 11:02:27 +0300

    Records class refactorinng

--- aiochclient/records.py
+++ aiochclient/records.py
@@ -1,8 +1,6 @@
 from collections.abc import Mapping
 from typing import Any, Callable, Dict, Iterator, List, Tuple, Union
 
-from aiochclient.exceptions import ChClientError
-
 # Optional cython extension:
 try:
     from aiochclient._types import what_py_converter
@@ -32,25 +30,20 @@ class Record(Mapping):
 
     __slots__ = ("_converters", "_decoded", "_names", "_row")
 
-    def __init__(
-        self, row: Tuple[bytes], names: Dict[str, Any], converters: List[Callable]
-    ):
-        self._row: Tuple[Any] = row
+    def __init__(self, row: bytes, names: Dict[str, Any], converters: List[Callable]):
+        self._row: Union[bytes, Tuple[Any]] = row
         if not self._row:
             # in case of empty row
             self._decoded = True
             self._converters = []
             self._names = {}
         else:
-            if len(row) != len(names):
-                raise ChClientError("Incorrect response from ClickHouse")
             self._decoded = False
             self._converters = converters
             self._names = names
 
     def __getitem__(self, key: Union[str, int, slice]) -> Any:
-        if not self._decoded:
-            self._decode()
+        self._decode()
         if type(key) == str:
             try:
                 return self._row[self._names[key]]
@@ -73,10 +66,15 @@ class Record(Mapping):
         return iter(self._names)
 
     def __len__(self) -> int:
-        return len(self._row)
+        return len(self._names)
 
     def _decode(self):
-        self._row = tuple(typ(val) for typ, val in zip(self._converters, self._row))
+        if self._decoded:
+            return None
+        self._row = tuple(
+            converter(val)
+            for converter, val in zip(self._converters, self._row.split(b"\t"))
+        )
         self._decoded = True
 
 
@@ -91,10 +89,9 @@ class RecordsFabric:
             what_py_converter(tp) for tp in tps.decode().strip().split("\t")
         ]
 
-    def new(self, vls: bytes) -> Record:
-        vls = vls[:-1]
+    def new(self, row: bytes) -> Record:
         return Record(
-            row=tuple(vls.split(b"\t")) if vls else (),
+            row=row[:-1],  # because of delimiter
             names=self.names,
             converters=self.converters,
         )
